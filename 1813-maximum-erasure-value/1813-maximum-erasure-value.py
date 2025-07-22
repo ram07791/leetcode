ffbf3318095f124c9c55f757c98ab6e2367dbdea
Dattaram tellapati <ramzelda861@gmail.com>
2025-07-22 12:50:34 +0530

    Time: 367 ms (16.13%), Space: 20.3 MB (83.23%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        l = 0
+        r = 0
+        n = len(nums)
+        freq = {}
+        total = 0
+        max_sum = 0
+
+        while r < n:
+            freq[nums[r]] = freq.get(nums[r], 0) + 1
+            total += nums[r]
+
+            while (r - l + 1) > len(freq):
+                freq[nums[l]] -= 1
+                total -= nums[l]
+                if freq[nums[l]] == 0:
+                    del freq[nums[l]]
+                l += 1
+
+            max_sum = max(max_sum, total)
+            r += 1
+
+        return max_sum
+        
