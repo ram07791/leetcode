3213f2543e03f2eb79f9a04f141c62601c2a144d
Dattaram tellapati <ramzelda861@gmail.com>
2025-08-01 17:12:44 +0530

    Time: 7 ms (4.76%), Space: 12.5 MB (21.66%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,21 @@
 class Solution(object):
+    def factorial(self, n):
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+    def nCr(self, n, r):
+        return self.factorial(n) // (self.factorial(r) * self.factorial(n - r))
     def generate(self, numRows):
         """
         :type numRows: int
         :rtype: List[List[int]]
         """
         triangle = []
-
-        for i in range(numRows):
-            row = [1] * (i+1)
-
-            for j in range(1,i):
-                row[j] = triangle[i-1] [j-1] +triangle[i-1][j]
+        for n in range(numRows):
+            row = []
+            for r in range(n + 1):
+                row.append(self.nCr(n, r))
             triangle.append(row)
         return triangle
-        
