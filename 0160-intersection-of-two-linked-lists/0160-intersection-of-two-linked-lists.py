174337fde3995e2e6e2e372155601b411d54396b
Dattaram tellapati <ramzelda861@gmail.com>
2026-01-30 20:53:30 +0530

    Time: 252 ms (58.42%), Space: 66.8 MB (68.4%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -12,7 +12,7 @@ class Solution(object):
         """
         t1 = headA
         t2 = headB
-        while(t1 is not t2):
+        while(t1 !=t2):
             t1 = headB if t1 is  None else t1.next
             t2 = headA if t2 is None  else t2.next
         return t1
